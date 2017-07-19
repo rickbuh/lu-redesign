14e5ac15e27ef5a2b17e1dafad7ad498ce973053
Rick Buncak <srbuncak@gmail.com>
2017-07-18 23:44:28 -0700

    Attemped to add dynamic elements
    Having issues with grayscale scroll function

--- functions.js
+++ functions.js
@@ -1,9 +1,27 @@
-// when open-nav is clicked pull in navbar from the right
+var vertScroll = window.pageYOffset;
+var navbar = document.getElementById("navbar");
+var recentImage = document.getElementById("recent-img");
+var grayValue = 1 - (vertScroll/500);
+
 function openNav() {
-    document.getElementById("navbar").style.right = "0%";
+    navbar.style.right = "0%";
 }
 
 // when close-nav is clicked reset navbar back off screen
 function closeNav() {
-    document.getElementById("navbar").style.right = "-50%";
+    navbar.style.right = "-50%";
 }
+
+// remove grayscale filter from recent-img when central on page
+window.addEventListener('scroll', function(){
+  var vertScroll = window.pageYOffset;
+  var grayValue = 100 - (vertScroll/5);
+  console.log(grayValue);
+  if (vertScroll > 500) {
+    var grayValue = 0;
+  }
+  function imageGrayscale() {
+    recentImage.style.filter = "grayscale("+grayValue+"%)";
+  }
+
+})
