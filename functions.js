50ebee765260f586a523c01749f81d3ace72ece0
Rick Buncak <srbuncak@gmail.com>
2017-07-18 20:11:43 -0700

    Added media queries and mobile nav functioning

---
+++ functions.js
@@ -0,0 +1,9 @@
+// when open-nav is clicked pull in navbar from the right
+function openNav() {
+    document.getElementById("navbar").style.right = "0%";
+}
+
+// when close-nav is clicked reset navbar back off screen
+function closeNav() {
+    document.getElementById("navbar").style.right = "-50%";
+}
