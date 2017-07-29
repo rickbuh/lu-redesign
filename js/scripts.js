cf3c2460f99f1d52ddf36980ab2a83f64aaa48a3
Rick Buncak <srbuncak@gmail.com>
2017-07-29 01:36:14 -0700

    Got Roster Page transition working
    Changed some roster css
    Add jekyll to gitignore

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,8 @@
----
----
-
 // JavaScript Document
 
 var nav = document.getElementById("navbar");
+var rosterPages = document.getElementsByClassName("roster-page");
+var pageNum = document.getElementsByClassName("pageNum");
 
 document.getElementById("open-nav").onclick = function() {
   nav.style.right = '0';
@@ -11,4 +10,32 @@ document.getElementById("open-nav").onclick = function() {
 
 document.getElementById("close-nav").onclick = function() {
   nav.style.right = '-50%';
+};
+
+function roster1() {
+  document.getElementById("roster-1").style.display = "grid";
+  document.getElementById("roster-2").style.display = "none";
+  document.getElementById("roster-3").style.display = "none";
+  document.getElementById("roster-4").style.display = "none";
+};
+
+function roster2() {
+  document.getElementById("roster-1").style.display = "none";
+  document.getElementById("roster-2").style.display = "grid";
+  document.getElementById("roster-3").style.display = "none";
+  document.getElementById("roster-4").style.display = "none";
+};
+
+function roster3() {
+  document.getElementById("roster-1").style.display = "none";
+  document.getElementById("roster-2").style.display = "none";
+  document.getElementById("roster-3").style.display = "grid";
+  document.getElementById("roster-4").style.display = "none";
+};
+
+function roster4() {
+  document.getElementById("roster-1").style.display = "none";
+  document.getElementById("roster-2").style.display = "none";
+  document.getElementById("roster-3").style.display = "none";
+  document.getElementById("roster-4").style.display = "grid";
 };
